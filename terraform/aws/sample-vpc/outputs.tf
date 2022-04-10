5619d5ad54f59a4eeaa0f2918fb7bfdb115dd524
hernanku <akuhernandez@outlook.com>
2022-04-10 10:46:49 -0400

    updating vpc vars

--- terraform/aws/sample-vpc/outputs.tf
+++ terraform/aws/sample-vpc/outputs.tf
@@ -3,18 +3,18 @@ output "vpc_id" {
 }
 
 output "private_subnets" {
-    value = module.vpc.private_subnets
+  value = module.vpc.private_subnets
 }
 
 output "private_subnets_cidrs" {
-    value = module.vpc.private_subnets_cidr_blocks
+  value = module.vpc.private_subnets_cidr_blocks
 }
 
 output "public_subnets" {
-    value = module.vpc.public_subnets
+  value = module.vpc.public_subnets
 }
 
 output "public_subnets_cidrs" {
-    value = module.vpc.public_subnets_cidr_blocks
+  value = module.vpc.public_subnets_cidr_blocks
 }
 
