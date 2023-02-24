2b64e43d4780b8b9342b533b47ce277cdff1dbeb
Hernandez Anyiabey <akuhernandez@outlook.com>
2023-02-23 23:07:52 -0500

    terraform vm disk size, openvpn conector server infra

---
+++ openvpn-connector/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "local" {}
+}
+
+
+module "vm" {
+  source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
+  vsphere_server_url = "10.0.0.31"
+  vsphere_user       = "administrator@vsphere.local"
+  vsphere_password   = ""
+  vsphere_datacenter = "dc-east"
+  vsphere_cluster    = "dev-cluster"
+  vm_datastore = "vmData"
+  vm_name            = "openvpn-connector"
+  vm_domain = "labz.io"
+  vm_ipv4_address = "10.0.0.27"
+  vm_gateway = "10.0.0.1"
+  vm_dns_server = "10.0.0.33"
+  // vm_count = 1
+  vm_network     = "vmPub"
+  vm_template    = "base-ubuntu22-templ"
+  vm_cpu         = 1
+  vm_memory      = 2048
+}
+
